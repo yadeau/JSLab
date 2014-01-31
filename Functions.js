0776994ab3ba34562dde49a5e8fdf824299f711c
walco005 <walco005@morris.umn.edu>
2014-01-31 16:13:58 -0600

    FINALLY got things to work...

--- Functions.js
+++ Functions.js
@@ -1,1 +1,30 @@
 
+function initialize() {
+    var queue = [];
+    var position = 0;
+    return {
+        save: function (input) {
+            if(queue.length == 5) {
+                queue.shift();
+            }
+            queue.push(input);
+            position++;
+        },
+        undo: function () {
+            if(position != 0) {
+                position--;
+                var toReturn = queue[position];
+                return toReturn;
+            }
+            return "Invalid use of undo"
+        },
+        redo: function () {
+            if(position != 5) {
+                position++;
+                var toReturn = queue[position];
+                return toReturn;
+            }
+            return "Invalid use of redo"
+        }
+    }
+}
