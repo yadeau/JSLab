2fefb8aa1d63dd1fb98a12d0a68f45b4cd00b4e8
yadeau <yadea003@morris.umn.edu>
2014-02-02 21:50:12 -0600

    WE ARE DONE!

--- Functions.js
+++ Functions.js
@@ -8,7 +8,9 @@ function initialize() {
                 queue.shift();
             }
             queue.push(input);
-            position++;
+            if(position < 5){
+                position++;
+            }
         },
         undo: function () {
             if(position != 0) {
@@ -19,15 +21,20 @@ function initialize() {
             return "Invalid use of undo"
         },
         redo: function () {
-            if(position != 5) {
-                position++;
+            if(position < 5) {
                 var toReturn = queue[position];
+                if(position != 5){
+                    position++;
+                }
                 return toReturn;
             }
             return "Invalid use of redo"
         },
         getQueue: function (input) {
             return queue[input];
+        },
+        getPosition: function () {
+            return position;
         }
     }
 }
--- Testing.js
+++ Testing.js
@@ -10,36 +10,72 @@ describe('initialize', function(){
     describe('Adding test', function(){
         // These break if we have anything besides what numbers are there
         it('Should store 0 in queue at index 0.', function(){
-            testSubject.save("0");
-            assert.equal("0", testSubject.getQueue(0));
+            testSubject.save("A");
+            assert.equal("A", testSubject.getQueue(0));
         })
         it('Should store 1 in queue at index 1.', function(){
-            testSubject.save("1");
-            assert.equal("1", testSubject.getQueue(1));
+            testSubject.save("B");
+            assert.equal("B", testSubject.getQueue(1));
         })
         it('Should store 2 in queue at index 2.', function(){
-            testSubject.save("2");
-            assert.equal("2", testSubject.getQueue(2));
+            testSubject.save("C");
+            assert.equal("C", testSubject.getQueue(2));
         })
         it('Should store 3 in queue at index 3.', function(){
-            testSubject.save("3");
-            assert.equal("3", testSubject.getQueue(3));
+            testSubject.save("D");
+            assert.equal("D", testSubject.getQueue(3));
         })
         it('Should store 4 in queue at index 4.', function(){
-            testSubject.save("4");
-            assert.equal("4", testSubject.getQueue(4));
+            testSubject.save("E");
+            assert.equal("E", testSubject.getQueue(4));
         })
         it('Should store 5 in queue at index 4.', function(){
-            testSubject.save("5");
-            assert.equal("5", testSubject.getQueue(4));
-        })
-        it('Should store 1 in queue at index 0.', function(){
-            assert.equal("1", testSubject.getQueue(0));
+            testSubject.save("F");
+            assert.equal("F", testSubject.getQueue(4));
         })
         describe('Undo test', function(){
             it('Should move down the list', function(){
-                assert.equal("4", testSubject.undo());
+                assert.equal("F", testSubject.undo());
             })
+            it('Should return E', function(){
+                assert.equal("E", testSubject.undo());
+            });
+            it('Should return D', function(){
+                assert.equal("D", testSubject.undo());
+            });
+            it('Should return C', function(){
+                assert.equal("C", testSubject.undo());
+            });
+            it('Should return B', function(){
+                assert.equal("B", testSubject.undo());
+            });
+            it('Should return Invalid', function(){
+                assert.equal("Invalid use of undo", testSubject.undo());
+            });
+        });
+
+        describe('Redo test', function(){
+            it('Should return B', function(){
+                assert.equal("B", testSubject.redo());
+            })
+            it('Should return C', function(){
+                assert.equal("C", testSubject.redo());
+            });
+            it('Should return D', function(){
+                assert.equal("D", testSubject.redo());
+            });
+            it('Should return E', function(){
+                assert.equal("E", testSubject.redo());
+            });
+            it('Should return F', function(){
+                assert.equal("F", testSubject.redo());
+            });
+            it('should return 5', function(){
+                assert.equal("5", testSubject.getPosition());
+            });
+            it('Should return Invalid', function(){
+                assert.equal("Invalid use of redo", testSubject.redo());
+            });
         });
     });
 });
